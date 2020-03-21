8e05c1fe6737cbdab9386fe6f2f59024e4e073ed
YashMunjal <yashmu45@gmail.com>
2020-03-22 03:57:18 +0530

    user-media issue

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-const video=document.getElementById('video');
-function startVideo(){
-  navigator.getUserMedia({video},stream=>(video.srcObject = stream),
-  err=>console.log(err)
+const video = document.getElementById('video');
+navigator.getWebcam = (navigator.getUserMedia || navigator.webKitGetUserMedia || navigator.moxGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+function startVideo() {
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video }, stream => (video.srcObject = stream),
+      err => console.log(err)
+    )
+  }
+  else {
+    navigator.getWebcam({ video }, stream => (video.srcObject = stream),
+    err => console.log(err)
   )
+  }
 }
 const audio = document.getElementById('audio');
 
@@ -14,25 +22,24 @@ Promise.all([
   faceapi.nets.ageGenderNet.loadFromUri("./models")
 ]).then(startVideo);
 
-video.addEventListener("playing",()=>
-{
+video.addEventListener("playing", () => {
   const canvas = faceapi.createCanvasFromMedia(video);
   document.body.append(canvas);
-  const displaySize = {width:video.width,height:video.height};
-  faceapi.matchDimensions(canvas,displaySize)
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize)
 
 
-//audios
-const happy_audios = ['./audios/happy_audio/2.mp3','./audios/happy_audio/1.mp3'];
-const sad_sounds = ['./audios/sad_sounds/1.mp3'];
-const angry_audios = ['./audios/angry_audio/1.mp3']; 
+  //audios
+  const happy_audios = ['./audios/happy_audio/2.mp3', './audios/happy_audio/1.mp3'];
+  const sad_sounds = ['./audios/sad_sounds/1.mp3'];
+  const angry_audios = ['./audios/angry_audio/1.mp3'];
 
-var flag_happy=0;
-var flag_sad=1000;
-var flag_angry=5000;
-var flag_surprised=10000;
+  var flag_happy = 0;
+  var flag_sad = 1000;
+  var flag_angry = 5000;
+  var flag_surprised = 10000;
   //face-setInterval
-setInterval(async () => {
+  setInterval(async () => {
     const detections = await faceapi
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceExpressions()
@@ -41,68 +48,59 @@ setInterval(async () => {
 
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     //expression logic
-    const happy  =resizedDetections[0].expressions.happy;
-    const sad  =resizedDetections[0].expressions.sad;
-    const surprised  =resizedDetections[0].expressions.surprised;
-    const disgusted  =resizedDetections[0].expressions.disgusted;
-    const angry  =resizedDetections[0].expressions.angry;
-    const neutral  =resizedDetections[0].expressions.neutral;
+    const happy = resizedDetections[0].expressions.happy;
+    const sad = resizedDetections[0].expressions.sad;
+    const surprised = resizedDetections[0].expressions.surprised;
+    const disgusted = resizedDetections[0].expressions.disgusted;
+    const angry = resizedDetections[0].expressions.angry;
+    const neutral = resizedDetections[0].expressions.neutral;
     //The Issue
-    if(happy>0.9)
-    {
+    if (happy > 0.9) {
       flag_happy++;
-      playAudio(happy_audios[Math.floor(Math.random()*happy_audios.length)],flag_happy);
+      playAudio(happy_audios[Math.floor(Math.random() * happy_audios.length)], flag_happy);
     }
-    
-    else if(angry>0.75)
-    {
+
+    else if (angry > 0.75) {
       flag_angry++;
-      playAudio(angry_audios[Math.floor(Math.random()*angry_audios.length)],flag_angry);
+      playAudio(angry_audios[Math.floor(Math.random() * angry_audios.length)], flag_angry);
     }
-    else if(neutral>0.6 || detections.length==0)
-    {
-      playAudio("",0);
+    else if (neutral > 0.6 || detections.length == 0) {
+      playAudio("", 0);
     }
-    else if(sad>0.8)
-    {
+    else if (sad > 0.8) {
       flag_sad++;
-      playAudio(sad_sounds[Math.floor(Math.random()*sad_sounds.length)],flag_sad)
+      playAudio(sad_sounds[Math.floor(Math.random() * sad_sounds.length)], flag_sad)
     }
     faceapi.draw.drawDetections(canvas, resizedDetections);
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-  },200)
-  function playAudio(emit,f)
-  {
-    if(f==1)
-    {
+  }, 200)
+  function playAudio(emit, f) {
+    if (f == 1) {
       //happy
-    audio.src=emit;
-    flag_sad=1000;
-    flag_angry=5000;
-    audio.play();
+      audio.src = emit;
+      flag_sad = 1000;
+      flag_angry = 5000;
+      audio.play();
     }
-    else if(f==1001)
-    {
+    else if (f == 1001) {
       //sad
-      audio.src=emit;
-      flag_happy=0;
-      flag_angry=5000;
+      audio.src = emit;
+      flag_happy = 0;
+      flag_angry = 5000;
       audio.play();
     }
-    else if(f==5001)
-    {
+    else if (f == 5001) {
       //angry
-      audio.src=emit;
-      flag_happy=0;
-      flag_sad=1000;
+      audio.src = emit;
+      flag_happy = 0;
+      flag_sad = 1000;
       audio.play();
     }
-    else if(f==0)
-    {
-      audio.src="";
-      flag_happy=0;
-      flag_sad=1000;
-      flag_angry=5000;
+    else if (f == 0) {
+      audio.src = "";
+      flag_happy = 0;
+      flag_sad = 1000;
+      flag_angry = 5000;
     }
   }
 })
